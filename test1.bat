d450b6af729a8f61d957a3b2a26188a57b6ae22c
Pratham28 <84891149+PrathamP28@users.noreply.github.com>
2025-08-28 19:03:44 +0530

    Update test1.bat

--- test1.bat
+++ test1.bat
@@ -4,3 +4,11 @@ set path="C:\Program Files\MyPythonTrayApp\main.exe"
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v %appname% /t REG_SZ /d %path% /f
 echo [%appname%] added to startup with path: %path%
 pause
+
+
+@echo off
+set appname=main
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v %appname% /f
+
+echo [%appname%] removed from startup
+pause
