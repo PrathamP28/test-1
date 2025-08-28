f1c74c15fe97f85d35548208c9b7d4387f87bcab
Pratham28 <84891149+PrathamP28@users.noreply.github.com>
2025-08-28 19:02:46 +0530

    Update test1.bat

--- test1.bat
+++ test1.bat
@@ -1,10 +1,6 @@
 @echo off
-:: Change these values to your app details
-set AppName=MyPythonTrayApp
-set AppPath="C:\Program Files\MyPythonTrayApp\main.exe"
-
-:: Add registry key for startup
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v %AppName% /t REG_SZ /d %AppPath% /f
-
-echo [%AppName%] added to startup with path: %AppPath%
+set appname=main
+set path="C:\Program Files\MyPythonTrayApp\main.exe"
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v %appname% /t REG_SZ /d %path% /f
+echo [%appname%] added to startup with path: %path%
 pause
